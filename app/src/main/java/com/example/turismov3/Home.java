5d182e0affae675f7fa2625cfd2f6f8138ae1425
arroyavepjuan <jarroyavepa@cesde.net>
2023-07-04 20:39:33 -0500

    creación menú de opciones

--- app/src/main/java/com/example/turismov3/Home.java
+++ app/src/main/java/com/example/turismov3/Home.java
@@ -3,6 +3,8 @@ package com.example.turismov3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -12,7 +14,7 @@ public class Home extends AppCompatActivity {
     //ATRIBUTOS
     Button botonHoteles;
     Button botonRestaurantes;
-    Button botonTurismo;
+    Button botonSitios;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +23,7 @@ public class Home extends AppCompatActivity {
 
         botonHoteles=findViewById(R.id.botonhoteles);
         botonRestaurantes=findViewById(R.id.botonrestaurantes);
-        botonTurismo=findViewById(R.id.botonsitios);
+        botonSitios=findViewById(R.id.botonsitios);
 
         // DETECTANDO EVENTOS
 
@@ -31,5 +33,42 @@ public class Home extends AppCompatActivity {
                 Toast.makeText(Home.this, "di click", Toast.LENGTH_SHORT).show();
             }
         });
+
+        botonRestaurantes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Home.this, "di click", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        botonRestaurantes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Home.this, "di click", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        int itemSeleccionado=item.getItemId();
+        switch (itemSeleccionado){
+            case(R.id.opcion1):
+                break;
+            case (R.id.opcion2):
+                break;
+            case (R.id.opcion3):
+                break;
+            case (R.id.opcion4):
+                break;
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
