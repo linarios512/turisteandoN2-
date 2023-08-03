d71bb35734fee2c19f73ff1be35a4835f9c0de25
r3l4x1n <nfzapataca@cesde.net>
2023-08-02 20:49:51 -0500

    creacion de moldes

---
+++ app/src/main/java/com/example/turismov3/moldes/MoldeHotel.java
@@ -0,0 +1,52 @@
+package com.example.turismov3.moldes;
+
+public class MoldeHotel {
+
+    private String nombre;
+    private String precio;
+    private String telefono;
+    private Integer foto;
+
+    public MoldeHotel() {  //constructor vacio
+    }
+
+    public MoldeHotel(String nombre, String precio, String telefono, Integer foto) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.telefono = telefono;
+        this.foto = foto;
+
+    } // constructor lleno
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Integer getFoto() {
+        return foto;
+    }
+
+    public void setFoto(Integer foto) {
+        this.foto = foto;
+    }
+}
